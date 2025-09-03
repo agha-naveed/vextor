d887c676b786245bebc9d0a9fe86fc8ee27a562d
agha-naveed <naveedabs31@gmail.com>
2025-09-03 13:13:59 +0500

    mode toggle

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,23 @@
 "use client"
-
+import { useEffect, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 import logo from "@/images/logo.png"
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+import { useThemeContext } from "./ThemeContext"
 
 export default function Navbar() {
+    // const [isDarkMode, setDarkMode] = useState(false);
+    
+    const { isDarkMode, toggleDarkMode } = useThemeContext();
+
+    // const toggleDarkMode = (checked: boolean) => {
+    //     setDarkMode(checked);
+    // };
+    useEffect(() => {
+        console.log(isDarkMode)
+    }, [isDarkMode]);
+
     return (
         <nav className="fixed top-0 text-white flex justify-between w-full px-10 py-8">
             <div className="container mx-auto flex justify-between items-center">
@@ -12,9 +25,7 @@ export default function Navbar() {
                     <Image src={logo} alt="Vextor AI Logo" className="select-none w-[150px]" />
                 </Link>
                 <ul className="flex gap-10">
-                    <li style={{
-                        // textShadow: "0 0 10px white",
-                    }}>
+                    <li>
                         <Link href="/" className="transition-all hover:text-shadow-[0_0_25px_#ffffff87]">Docs</Link>
                     </li>
                     <li>
@@ -27,12 +38,21 @@ export default function Navbar() {
                         <Link href="/" className="transition-all hover:text-shadow-[0_0_25px_#ffffff87]">FAQ</Link>
                     </li>
                 </ul>
-                <div className="relative inline-block z-10 group">
-                    <div style={{transition: "0.2s linear"}} className="absolute inset-0 z-[-1] rounded-md blur-xl bg-gradient-to-r from-[#3185ff] to-[#bc5bf9] opacity-0 group-hover:opacity-50"></div>
+                <div className="flex gap-5 items-center max-h-[40px]">
+                    <DarkModeSwitch
+                        style={{ marginBottom: '2rem' }}
+                        checked={isDarkMode}
+                        onChange={toggleDarkMode}
+                        size={26}
+                        className="translate-y-4 text-white"
+                    />
+                    <div className="relative inline-block z-10 group">
+                        <div style={{transition: "0.2s linear"}} className="absolute inset-0 z-[-1] rounded-md blur-xl bg-gradient-to-r from-[#3185ff] to-[#bc5bf9] opacity-0 group-hover:opacity-50"></div>
 
-                    <button className="bg-gradient-to-r from-[#3185ff] to-[#bc5bf9] py-[7px] px-[18px] rounded-md bg-[length:200%_100%] bg-left hover:bg-right transition-all duration-500 cursor-pointer text-white">
-                        Download
-                    </button>
+                        <button className="bg-gradient-to-r from-[#3185ff] to-[#bc5bf9] py-[7px] px-[18px] rounded-md bg-[length:200%_100%] bg-left hover:bg-right transition-all duration-500 cursor-pointer text-white">
+                            Download
+                        </button>
+                    </div>
                 </div>
             </div>
         </nav>
