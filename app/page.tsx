45abc943c9f7fac335d1f818f6dcd1a3674abeea
Syed Naveed Abbas <naveedabs31@gmail.com>
2025-10-19 20:14:48 +0500

    flare position changed

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,27 @@ import Image from "next/image";
 export default function Page() {
   const { isDarkMode } = useThemeContext();
   return (
-    <div className={`${isDarkMode ? "bg-main-dark" : "bg-white"} w-full relative`}>
-      <div className="w-full min-h-screen absolute top-0 left-0 " style={{backgroundImage: "linear-gradient(135deg, rgb(243, 244, 246) 0%, rgb(229, 231, 235) 25%, rgb(215, 215, 219) 50%, rgb(40, 44, 52) 100%)"}}></div>
-      <div className="absolute inset-0"
+    <div className={`${isDarkMode ? "bg-main-dark" : "bg-white"} w-full relative overflow-hidden`}>
+      {
+        isDarkMode ?
+        <div className="w-full min-h-screen absolute top-0 left-0 " style={{backgroundImage: "linear-gradient(135deg, rgb(19, 19, 19) 0%, rgb(35, 36, 37) 25%, transparent 50%, black 100%)"}}></div>
+        :
+        <div className="w-full min-h-screen absolute top-0 left-0 " style={{backgroundImage: "linear-gradient(135deg, rgb(243, 244, 246) 0%, rgb(229, 231, 235) 25%, rgb(215, 215, 219) 50%, rgb(40, 44, 52) 100%)"}}></div>
+        
+      }
+      <div className="absolute inset-0 opacity-20"
         style={{backgroundImage: "linear-gradient(to right, rgba(220, 220, 230, 0.15) 1px, transparent 1px), linear-gradient(rgba(220, 220, 230, 0.15) 1px, transparent 1px)", backgroundSize: "30px 30px"}}></div>
+
       <header className="container mx-auto h-full min-h-screen flex flex-col justify-center items-center relative z-[99999]">
         
         <div className="relative text-center">
           <div className="relative z-20">
+            
             <h1 className={`text-[80px] font-bold ${isDarkMode ? "text-white" : "text-black"}`}>Vextor AI Code Editor</h1>
+
+            <Image src={flare} alt="flare" loading="lazy" className={`absolute -top-80 -right-90 ${isDarkMode ? "animate-flare-fade" : "animate-dark-flare-fade"} w-full h-auto select-none z-0 outline-none`} />
+            
+
             <h4 className={`uppercase text-[28px] ${isDarkMode ? "text-white" : "text-black"} relative -top-1 tracking-wider`}>Shift your coding direction</h4>
             <div className="relative inline-block z-10 group mt-7" title="Download Vextor for Windows">
               <div style={{transition: "0.2s linear"}} className="absolute inset-0 z-[-1] rounded-md blur-xl bg-gradient-to-r from-[#3185ff] to-[#bc5bf9] opacity-0 group-hover:opacity-20"></div>
@@ -25,7 +37,6 @@ export default function Page() {
               </button>
             </div>
           </div>
-          <Image src={flare} alt="flare" loading="lazy" className={`absolute -top-40 ${isDarkMode ? "animate-flare-fade" : "animate-dark-flare-fade"} left-1/2 -translate-x-1/2 w-full h-auto select-none z-0 outline-none`} />
         </div>
       </header>
     </div>
