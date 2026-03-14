8bfd8911af675dce162cc212f130a7942e7a71bd
Syed Naveed Abbas <naveedabs31@gmail.com>
2026-03-14 22:03:09 +0500

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+"use client"
+import { useTheme } from "next-themes"
 
 export default function page() {
+  const { theme, setTheme } = useTheme()
+
   return (
-    <div>page</div>
+    <div className="bg-main h-screen grid place-content-center">
+      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")} className="px-3 py-1 bg-white rounded-md cursor-pointer text-black">Click Me</button>
+    </div>
   )
 }
