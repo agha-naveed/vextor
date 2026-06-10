9f6cdad75e7ce8fc02ea31ee2b030796405dc3af
Syed Naveed Abbas <naveedabs31@gmail.com>
2026-06-10 14:14:23 +0500

    added sitemap and robot

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://vextor.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://vextor.vercel.app/docs',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+  ]
+}
